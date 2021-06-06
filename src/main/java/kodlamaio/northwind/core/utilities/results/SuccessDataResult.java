51ea42e456c75fdf4b695a02a8d05c2a2c6404f1
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2021-06-06 17:32:01 +0300

    Initial commit

---
+++ src/main/java/kodlamaio/northwind/core/utilities/results/SuccessDataResult.java
@@ -0,0 +1,22 @@
+package kodlamaio.northwind.core.utilities.results;
+
+import javax.xml.crypto.Data;
+
+public class SuccessDataResult<T> extends DataResult {
+
+    public SuccessDataResult(Object data, String message) {
+        super(data, true, message);
+    }
+
+    public SuccessDataResult(T data) {
+        super(data, true);
+    }
+
+    public SuccessDataResult(String message) {
+        super(null, true, message);
+    }
+
+    public SuccessDataResult() {
+        super(null, true);
+    }
+}
---
+++ src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,30 @@
+package kodlamaio.northwind.dataAccess.abstracts;
+
+import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductDao extends JpaRepository<Product, Integer> {
+
+    Product getByProductName(String productName);
+
+    Product getByProductNameAndCategory_CategoryId(String ProductName, int categoryId);
+
+    List<Product> getByProductNameOrCategory_CategoryId(String ProductName, int categoryId);
+
+    List<Product> getByCategory_CategoryIdIn(List<Integer> categories);
+
+    List<Product> getByProductNameContains(String productName);
+
+    List<Product> getByProductNameStartsWith(String productName);
+
+    @Query("FROM Product where productName=:productName and category.categoryId=:categoryId")
+    List<Product> getByNameAndCategory(String productName, int categoryId);
+
+    @Query("SELECT new kodlamaio.northwind.entities.dtos.ProductWithCategoryDto(p.id,p.productName,c.categoryName) FROM Category c INNER JOIN c.products p")
+    List<ProductWithCategoryDto> getProductWithCategoryDetails();
+
+}
